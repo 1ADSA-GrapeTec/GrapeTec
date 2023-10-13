25f3fcbee7d97b98f2a8599bec215a8b53170507
amandaribeiro1 <amanda.costa@sptech.school>
2023-10-13 16:04:19 -0300

    Att Login e Dashboard

--- Algoritmos/Site Institucional/Site Amanda/public/script.js
+++ Algoritmos/Site Institucional/Site Amanda/public/script.js
@@ -16,3 +16,18 @@ function verificaSenha(){
         id_erroSenha.innerHTML = `Senha invalida`
     }
 }
+
+function login(){
+    const email = 'usuario@sptech.school'
+    const senha = 'sptech123'
+
+    var emailDigitado = id_email.value;
+    var senhaDigitada = id_senha.value;
+
+    if(emailDigitado == email && senhaDigitada == senha){
+        alert('Login efetuado!');
+        window.location.href = 'dashboard.html'; // window.location.href é uma propriedade JavaScript que contém o endereço completo (URL) da página atual no navegador. Ela é usada para acessar o endereço da página ou redirecionar o navegador para outro URL, controlando a navegação web.
+    }else{
+        alert('Usuario e/ou senha invalidos')
+    }
+}
